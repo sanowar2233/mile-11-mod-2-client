f6e5d241a1b99c1ed02ba8021010c04ffe5cdd65
sanowar2233 <sanowarhossain1082@gmail.com>
2022-10-30 11:14:02 +0600

    ad

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,18 +3,19 @@ import { useLoaderData } from 'react-router-dom';
 
 const Home = () => {
     const users= useLoaderData()
+    console.log(users)
 
     const handleDelete=(user)=>{
         const agree=window.confirm(`are you sure you want to delete:${user.name}`)
         console.log(agree)
         if(agree){
-            // console.log('deleting user ', user._id)
+            console.log('deleting user ', user._id)
 
-            fetch(`http://localhost:5000/users/${user._id}`,{
-                method:'DELETE'
-            })
-            .then(res=>res.json())
-            .then(data=>console.log(data))
+            // fetch(`http://localhost:5000/users/${user._id}`,{
+            //     method:'DELETE'
+            // })
+            // .then(res=>res.json())
+            // .then(data=>console.log(data))
            
 
         }
@@ -24,7 +25,7 @@ const Home = () => {
         <div>
             <h1>users: {users.length}</h1>
             {
-                users.map(user=><p key={user._id}>{user.name}  {user.email} {user._id} <button onClick={()=>handleDelete(user._id)}>X</button></p>)
+                users.map(user=><p key={user._id}>{user.name}  {user.email} <button onClick={()=>handleDelete(user)}>X</button></p>)
             }
         </div>
     );
